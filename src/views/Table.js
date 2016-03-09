720df47ecf0b36b3966a53b34b374130374d6026
Florian Scheibner <flo.de@hotmail.de>
2016-03-09 20:02:38 +0100

    ' -> ’

--- src/views/Table.js
+++ src/views/Table.js
@@ -95,7 +95,7 @@ export default class Table extends Component {
             <br/>
             <br/>
             <div>
-                L'écart:
+                L’écart:
                 <CardStack className="stack" cartes={jeu.pli}/>
                 <Nom nom={jeu.pli.map((p, i) => jeu.nomJoueurs[(jeu.tourDe + jeu.joueurs - (jeu.pli.length) + i) % jeu.joueurs])}/>
             </div>
