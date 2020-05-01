788ad9de76c621e6a44c2cda859c555813651688
Pierrick Gourlain <pgourlain@hotmail.com>
2020-05-01 17:01:31 +0200

    Add icons

--- src/server/main.tsx
+++ src/server/main.tsx
@@ -90,8 +90,23 @@ function reactHandler(req: IncomingMessage, res: ServerResponse) {
                 <meta charSet="utf-8"/>
                 <meta name="viewport" content="width=device-width"/>
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-                <link rel="icon" href={WEB_SERVER + '/img/icon.svg'}/>
-                <link rel="icon" sizes="192x192" href={WEB_SERVER + '/img/icon.png'}/>
+                <link rel="apple-touch-icon" sizes="57x57" href={WEB_SERVER + '/img/apple-icon-57x57.png'}/>
+                <link rel="apple-touch-icon" sizes="60x60" href={WEB_SERVER + '/img/apple-icon-60x60.png'}/>
+                <link rel="apple-touch-icon" sizes="72x72" href={WEB_SERVER + '/img/apple-icon-72x72.png'}/>
+                <link rel="apple-touch-icon" sizes="76x76" href={WEB_SERVER + '/img/apple-icon-76x76.png'}/>
+                <link rel="apple-touch-icon" sizes="114x114" href={WEB_SERVER + '/img/apple-icon-114x114.png'}/>
+                <link rel="apple-touch-icon" sizes="120x120" href={WEB_SERVER + '/img/apple-icon-120x120.png'}/>
+                <link rel="apple-touch-icon" sizes="144x144" href={WEB_SERVER + '/img/apple-icon-144x144.png'}/>
+                <link rel="apple-touch-icon" sizes="152x152" href={WEB_SERVER + '/img/apple-icon-152x152.png'}/>
+                <link rel="apple-touch-icon" sizes="180x180" href={WEB_SERVER + '/img/apple-icon-180x180.png'}/>
+                <link rel="icon" type="image/png" sizes="192x192"  href={WEB_SERVER + '/img/android-icon-192x192.png'}/>
+                <link rel="icon" type="image/png" sizes="32x32" href={WEB_SERVER + '/img/favicon-32x32.png'}/>
+                <link rel="icon" type="image/png" sizes="96x96" href={WEB_SERVER + '/img/favicon-96x96.png'}/>
+                <link rel="icon" type="image/png" sizes="16x16" href={WEB_SERVER + '/img/img/favicon-16x16.png'}/>
+                <link rel="manifest" href={WEB_SERVER + '/img/manifest.json'}/>
+                <meta name="msapplication-TileColor" content="#ffffff"/>
+                <meta name="msapplication-TileImage" content={WEB_SERVER + '/img/ms-icon-144x144.png'}/>
+                <meta name="theme-color" content="#ffffff"/>
                 <meta name="apple-mobile-web-app-capable" content="yes"/>
                 <link rel="apple-touch-icon" href={WEB_SERVER + '/img/icon.png'}/>
                 <style dangerouslySetInnerHTML={{__html: cssData}}/>
